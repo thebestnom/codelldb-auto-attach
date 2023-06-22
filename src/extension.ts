f07eeb4d19b1a987fd2e94f557f60ce97cad6e49
thebestnom <shoval.arad@gmail.com>
2023-06-23 00:33:06 +0300

    MVP working

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,23 @@
 import * as vscode from 'vscode';
+import psTree from "ps-tree";
 
 export function activate(context: vscode.ExtensionContext) {
 	vscode.debug.onDidReceiveDebugSessionCustomEvent((e)=>console.log(e));
 	vscode.debug.onDidStartDebugSession((debugSession)=>{
 		if(debugSession.type === "lldb" && debugSession.configuration.autoAttach) {
+			if(debugSession.configuration.pid) {
+				psTree(debugSession.configuration.pid, (err, children) => {
+					if(err) {
+						return;
+					} else {
+						children.forEach((a, i) => {vscode.debug.startDebugging(undefined,{
+							...debugSession.configuration,
+							pid: a.PID,
+							name: `${debugSession.configuration.name} child ${i}`
+						})});
+					}
+				});
+			}
 			console.log(debugSession);
 		} 
 	})	
